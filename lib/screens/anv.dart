0d5a862faf66962fceddb9ac01428507f5e828d6
eastdown <91318790+eastdown@users.noreply.github.com>
2021-10-28 01:05:54 +0900

    Update anv.dart
    import youtue player package
    +
    youtube player controller and the player itself

--- lib/screens/anv.dart
+++ lib/screens/anv.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'home.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class AnvPage extends StatefulWidget {
   const AnvPage({Key? key}) : super(key: key);
